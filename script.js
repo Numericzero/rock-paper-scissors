9639264621b51b67e6c2405b66d8bd82a9dcc00b
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-07-21 01:25:37 -0400

    Create getComputerChoice function
    The getComputerChoice function generates a random number between
    0, 1, and 2. Depending on the number, the function returns "rock",
    "paper", or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+
+function getComputerChoice()
+{
+    let choice = null;
+
+    let rng = (Math.random() * 1000) % 3;
+    
+    switch(rng)
+    {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+    }
+
+    return choice;
+}
