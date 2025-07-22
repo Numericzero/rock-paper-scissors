b830d4bbefe2845150958b26d5e8db17d7ecfae6
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-07-21 22:35:35 -0400

    Create and call playRound function
    The playRound function accepts humanSelection and
    computerSelection and evaluates the winner of the game. the
    corresponding score of the winner is incremented. If it is a tie,
    the score is not incremented.

--- script.js
+++ script.js
@@ -31,3 +31,47 @@ function getHumanChoice()
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice=="rock" & computerChoice=="scissors")
+    {
+        console.log("You win! Rock beats Scissors.");
+        humanScore++;
+    }
+    else if(humanChoice=="paper" & computerChoice=="rock")
+    {
+        console.log("You win! Paper beats Rock.");
+        humanScore++;
+    }
+    else if(humanChoice=="scissors" & computerChoice=="paper")
+    {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    }
+    else if(humanChoice=="rock" & computerChoice=="paper")
+    {
+        console.log("You lose! Paper beats Rock.");
+        computerScore++;
+    }
+    else if(humanChoice=="scissors" & computerChoice=="rock")
+    {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore++;
+    }
+    else if(humanChoice=="paper" & computerChoice=="scissors")
+    {
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    }
+    else
+    {
+        console.log("It's a tie!");
+    }
+}
+
+
+let humanSelection = getHumanChoice().toLowerCase();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
